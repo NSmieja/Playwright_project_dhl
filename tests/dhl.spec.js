c89654e5c9f50bb59f27806a8341c5e50becc992
Natalia Śmieja <n.smieja@sportradar.com>
2025-03-23 23:04:39 +0100

    refactoring to given-when-then scenarios

--- tests/dhl.spec.js
+++ tests/dhl.spec.js
@@ -8,60 +8,70 @@ const DHL_BASIC_URL = "https://support-developer.dhl.com/support/home"
 const FOTO_PATH = path.join(__dirname, '../screenshots/foto.png');
 
 
-test("Taking screenshot in playwright", async ({ page }) => {
-  await page.goto(DHL_BASIC_URL);
-
-  // Checking if the 'Knowledge base' link is visible.
+test("SCENARIO: Taking screenshot in playwright", async ({ page }) => {
   let locator = page.getByRole("link", { name: "Knowledge base" });
-  console.log(await locator.textContent());  // (with auto-wait mechanism)
-  await expect(locator).toHaveText("Knowledge base");
 
-  // Clicking the 'Knowledge base' link.
-  await locator.click();
+  await test.step("GIVEN: The user goes to the DHL page", async () => {
+    await page.goto(DHL_BASIC_URL);
+    // Checking if the 'Knowledge base' link is visible
+    console.log(await locator.textContent());  // (with auto-wait mechanism)
+    await expect(locator).toHaveText("Knowledge base");
+  });
 
-  // Checking if the 'General' link is visible.
-  let full_text = await page.locator(".line-clamp-2").first().textContent();
-  console.log(full_text);
-  expect(full_text).toContain("General");
+  await test.step("WHEN: User clicks on the 'Knowledge base' link", async () => {
+    await locator.click();
+    // Checking if the 'General' link is visible
+    let full_text = await page.locator(".line-clamp-2").first().textContent();
+    // console.log(full_text);
+    expect(full_text).toContain("General");
+  });
 
-  // Clicking the 'General' link.
-  await page.locator(".line-clamp-2").first().click();
+  await test.step("AND: User clicks on the 'General' link", async () => {
+    await page.locator(".line-clamp-2").first().click();
+    // Verifying if the page title is 'General'
+    await expect(page.locator(".fw-page-title")).toContainText("General");
+  });
 
-  // Taking a screenshot of the page.
-  await page.locator(".line-clamp-2").last().waitFor();  // waiting for the page to load
-  await page.screenshot({ path: FOTO_PATH, fullPage: true });
-  await page.waitForTimeout(5000);
-  expect(fs.existsSync(FOTO_PATH)).toBeTruthy();
-
+  await test.step("THEN: User can take a screenshot of the page and save in the 'screenshots' folder", async () => {
+    await page.locator(".line-clamp-2").last().waitFor();  // waiting for the page to load
+    await page.screenshot({ path: FOTO_PATH, fullPage: true });
+    await page.waitForTimeout(5000);
+    expect(fs.existsSync(FOTO_PATH)).toBeTruthy();
+  });
 });
 
-test("Navigating to the second page (bonus)", async ({ page }) => {
-  await page.goto(DHL_BASIC_URL);
 
-  // Checking if the 'Knowledge base' link is visible.
+test("SCENARIO: Navigating to the second page (bonus)", async ({ page }) => {
   let locator = page.getByRole("link", { name: "Knowledge base" });
-  console.log(await locator.textContent());  // (with auto-wait mechanism)
-  await expect(locator).toHaveText("Knowledge base");
 
-  // Clicking the 'Knowledge base' link.
-  await locator.click();
+  await test.step("GIVEN: The user goes to the DHL page", async () => {
+    await page.goto(DHL_BASIC_URL);
+    console.log(await locator.textContent());
+    await expect(locator).toHaveText("Knowledge base");
+  });
 
-  // Checking if the 'General' link is visible.
-  let full_text = await page.locator(".line-clamp-2").first().textContent();
-  console.log(full_text);
-  expect(full_text).toContain("General");
+  await test.step("WHEN: User clicks on the 'Knowledge base' link", async () => {
+    await locator.click();
+    let full_text = await page.locator(".line-clamp-2").first().textContent();
+    console.log(full_text);
+    expect(full_text).toContain("General");
+  });
 
-  // Clicking the 'General' link.
-  await page.locator(".line-clamp-2").first().click();
-  await expect(page.locator(".fw-page-title")).toContainText("General");
+  await test.step("AND: User clicks on the 'General' link", async () => {
+    await page.locator(".line-clamp-2").first().click();
+    await expect(page.locator(".fw-page-title")).toContainText("General");
+  });
 
-  // Clicking the '2' link.
-  await page.getByRole("link", { name: "2", exact: true }).click();
-  expect(page.url()).toContain("page/2");
+  await test.step("AND: User goes to the second page", async () => {
+    await page.getByRole("link", { name: "2", exact: true }).click();
+    expect(page.url()).toContain("page/2");
+  });
 
-  // Clicking in the first article.
-  await page.locator(".line-clamp-2").first().click();
-  await page.locator(".fw-page-title").waitFor();
-  await expect(page.locator(".fw-page-title")).toHaveText("What is DHL Freight?");
-  await expect(page.locator(".mb-8.mb-md-0.semi-bold.fs-20.fw-feedback-question")).toContainText("Was this article helpful?");
+  await test.step("THEN: User can go and read first article on that second page", async () => {
+    await page.locator(".line-clamp-2").first().click();
+    await page.locator(".fw-page-title").waitFor();
+    await expect(page.locator(".fw-page-title")).toHaveText("What is DHL Freight?");
+    await expect(page.locator(".mb-8.mb-md-0.semi-bold.fs-20.fw-feedback-question")).toContainText("Was this article helpful?");
+  });
+
 });
--- tests/dhl_shipment.spec.js
+++ tests/dhl_shipment.spec.js
@@ -1,44 +1,46 @@
 // @ts-check
-import { test, expect, request } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 
 const DHL_SHIPMENT_URL = "https://developer.dhl.com/api-reference/shipment-tracking#reference-docs-section/"
 const DHL_API = "https://api-test.dhl.com/track/"
 
-let tracking_number = "123";
-let service = "express";
-let language = "en";
-let shipment_payload = {
-  "trackingNumber": tracking_number,
-  "service": service,
-  "language": language
-};
 
+test("SCENARIO: Verifying shipment status directly from the web page", async ({ page }) => {
 
-test("Shipment status - from web page", async ({ page }) => {
-  // Navigating to the GET request for shipments
-  await page.goto(DHL_SHIPMENT_URL);
-  await page.locator(".opblock-summary-control").click();  // clicking on GET/shipments endpoint
-  await page.locator(".btn.try-out__btn").click();  // clicking on Try it out button
-  await expect(page.locator('.btn.try-out__btn.cancel')).toBeVisible();  // checking if the Cancel button is visible
+  await test.step("GIVEN: The user goes to the shipment web page", async () => {
+    await page.goto(DHL_SHIPMENT_URL);
+    await page.waitForLoadState("load", { timeout: 60000 });  // waiting for the page to load
+    await page.locator(".opblock-summary-control").click();  // clicking on GET/shipments endpoint
+    await page.locator(".btn.try-out__btn").click();  // clicking on Try it out button
+    await expect(page.locator('.btn.try-out__btn.cancel')).toBeVisible();  // checking if the Cancel button is visible    
+  });
 
-  // Filling in the required fields
-  await page.getByPlaceholder("trackingNumber").fill("123");
-  await page.locator(".btn.execute").click();
+  await test.step("WHEN: The user fills all required fields on the page with API example usage", async () => {
+    await page.getByPlaceholder("trackingNumber").fill("123");
+    await page.locator(".btn.execute").click();
+  });
 
-  // Checking the response status code
-  const correctRow = page.locator('tr[class="response"]');
-  const statusCell = correctRow.locator("td.response-col_status");
-  await expect(statusCell).toBeVisible();
-  await expect(statusCell).toHaveText("200");
+  await test.step("THEN: The user should see the 200 response", async () => {
+    let correct_row = page.locator('tr[class="response"]');
+    let status_cell = correct_row.locator("td.response-col_status");
+    await expect(status_cell).toBeVisible();
+    await expect(status_cell).toHaveText("200");
+  });
 
-  // Checking the country code
-  expect(await page.getByText('{ "shipments": [ { "id": "').textContent()).toContain("US");
-
+  await test.step("AND: The user should see 'US' in the country code field in the response body", async () => {
+    expect(await page.getByText('{ "shipments": [ { "id": "').textContent()).toContain("US");
+  });
 });
 
-test("Shipment status - from api request", async ({ request }) => {
-  // Navigating to the GET request for shipments
+
+test("SCENARIO: Verifying shipment status from the api request", async ({ request }) => {
+  // Given the user knows all the required data for the shipment
+  let tracking_number = "123";
+  let service = "express";
+  let language = "en";
+
+  // When the user provides all required data in the API GET request 
   var response = await request.get(
     `${DHL_API}/shipments?trackingNumber=${tracking_number}&service=${service}&${language}=en`,
     {
@@ -49,17 +51,13 @@ test("Shipment status - from api request", async ({ request }) => {
     },
   );
 
-  // Validating status code of the response
+  // Then the user should see the 200 response status code
   expect(response.status()).toBe(200);
 
-  // Getting the response body
+  // And the user should see "US" in the country code field in the response body
   let response_body = await response.json();
   // console.log(response_body);
-  // console.log(response_body["shipments"][0]["origin"]);
   // console.log(response_body["shipments"][0]["origin"]["address"]["countryCode"]);
-
-  // Validating coountry code
   expect(response_body["shipments"][0]["origin"]["address"]["countryCode"]).toBe("US");
-
 });
 
